8dbe1e19a0a1ba12005b1ddfa5d2fce0401cfdda
JEsidio <JEsidio@users.noreply.github.com>
2018-09-10 01:36:22 -0300

    Adding database

---
+++ database/dstmDatabaseScript.sql
@@ -0,0 +1,52 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+-- -----------------------------------------------------
+-- Schema dstmDatabase
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema dstmDatabase
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `dstmDatabase` DEFAULT CHARACTER SET utf8 ;
+USE `dstmDatabase` ;
+
+-- -----------------------------------------------------
+-- Table `dstmDatabase`.`user`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `dstmDatabase`.`user` (
+  `idUser` INT NOT NULL AUTO_INCREMENT COMMENT 'Identification of each user',
+  `userName` VARCHAR(255) NOT NULL COMMENT 'User name to show on the home page of the dstm app',
+  `userEmail` VARCHAR(250) NOT NULL COMMENT 'User e-mail for the login verification and possible password reset',
+  `userPassword` VARCHAR(250) NOT NULL COMMENT 'User password for the login verification',
+  PRIMARY KEY (`idUser`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `dstmDatabase`.`task`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `dstmDatabase`.`task` (
+  `idTask` INT NOT NULL AUTO_INCREMENT COMMENT 'Identification of each task, showed as task number on the home page table',
+  `task` VARCHAR(250) NOT NULL COMMENT 'Task itself',
+  `taskEnd` DATE NOT NULL COMMENT 'Task deadline',
+  `marker` VARCHAR(250) NULL COMMENT 'Marker to organize tasks based on user\'s need, for example: #school; #work; #home',
+  `inFinished` INT NOT NULL COMMENT 'Status of the task if it was finished or not, 0 for unfinished and 1 for finished',
+  `inStatus` INT NOT NULL COMMENT 'Status of the task if it was deleted or not, 0 for undeleted and 1 for deleted',
+  `user_idUser` INT NOT NULL COMMENT 'Foreign key of idUser to safely save tasks of each user',
+  PRIMARY KEY (`idTask`, `user_idUser`),
+  INDEX `fk_task_user1_idx` (`user_idUser` ASC),
+  CONSTRAINT `fk_task_user1`
+    FOREIGN KEY (`user_idUser`)
+    REFERENCES `dstmDatabase`.`user` (`idUser`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
